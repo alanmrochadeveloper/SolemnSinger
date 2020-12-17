a40321b01aac135ed1421deab42efcab8b5b3eb3
alanmrochadeveloper <54696320+alanmrochadeveloper@users.noreply.github.com>
2020-12-17 09:46:11 -0300

    Initial

---
+++ solemnsinger/ApplicationInformation.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace solemnsinger
+{
+    public class ApplicationInformation
+    {
+        public ApplicationInformation(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            Name = Configuration["ApplicationInformation:Name"];
+            URL = Configuration["ApplicationInformation:URL"];
+        }
+        private IConfiguration Configuration { get; }
+        public string Name { get; }
+        public string URL { get; }
+    }
+}
